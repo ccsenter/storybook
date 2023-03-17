beecfeb577827863bbcb38f3c23ecf950dbcd389
Csaba Csenteri <csaba.csenteri@oracle.com>
2023-03-17 03:20:45 +0000

    Adding docs support for preact renderer

--- code/renderers/preact/src/config.ts
+++ code/renderers/preact/src/config.ts
@@ -2,4 +2,6 @@ import { parameters as docsParams } from './docs/config';
 
 export { renderToCanvas, render } from './render';
 
+export { decorators, argTypesEnhancers } from './docs/config';
+
 export const parameters = { renderer: 'preact' as const, ...docsParams };
--- code/renderers/preact/src/docs/config.ts
+++ code/renderers/preact/src/docs/config.ts
@@ -1,5 +1,15 @@
+import { extractComponentDescription, enhanceArgTypes } from '@storybook/docs-tools';
+import { extractArgTypes } from './extractArgTypes';
+import { jsxDecorator } from './jsxDecorator';
+
 export const parameters = {
   docs: {
     story: { inline: true },
+    extractArgTypes,
+    extractComponentDescription,
   },
 };
+
+export const decorators = [jsxDecorator];
+
+export const argTypesEnhancers = [enhanceArgTypes];
